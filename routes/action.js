6edda6fe9665be39a4aae3776221935f887b6f6a
BackRunner <dev@backrunner.top>
2021-06-27 17:11:37 +0800

    feat: add hibernate action

--- routes/action.js
+++ routes/action.js
@@ -4,19 +4,30 @@ const { execSync } = require('child_process');
 
 router.prefix('/action');
 
-router.get('/shutdown', async(ctx, next)=>{
-    if (auth(ctx)){
-        execSync('shutdown -s -f -t 0');
-        ctx.body = 'Success';
-    } else {
-        ctx.status = 401;
-        ctx.body = 'Auth info is wrong.';
-    }
-    await next();
+router.get('/shutdown', async (ctx, next) => {
+  if (auth(ctx)) {
+    execSync('shutdown -s -f -t 0');
+    ctx.body = 'Success';
+  } else {
+    ctx.status = 401;
+    ctx.body = 'Auth info is wrong.';
+  }
+  await next();
 });
 
-function auth(ctx){
-   return ctx.get('Authorization') == config.auth_username+':'+config.auth_key
+router.get('/hibernate', async (ctx, next) => {
+  if (auth(ctx)) {
+    execSync('shutdown -s -h -t 0');
+    ctx.body = 'Success';
+  } else {
+    ctx.status = 401;
+    ctx.body = 'Auth info is wrong.';
+  }
+  await next();
+});
+
+function auth(ctx) {
+  return ctx.get('Authorization') == config.auth_username + ':' + config.auth_key;
 }
 
 module.exports = router;
